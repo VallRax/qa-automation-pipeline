1be8183a41def1e9704a2d3d7419ec4e5754b620
Jairo Alvarez S. <101760319+VallRax@users.noreply.github.com>
2026-05-27 11:57:45 -0400

    Update test_ui.py

--- tests/ui/test_ui.py
+++ tests/ui/test_ui.py
@@ -1,7 +1,5 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 
 def test_google():
     options = Options()
@@ -9,13 +7,8 @@ def test_google():
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
 
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=options
-    )
-
+    driver = webdriver.Chrome(options=options)
     driver.get("https://www.google.com")
 
     assert "Google" in driver.title
-
     driver.quit()
